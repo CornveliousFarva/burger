54a4c3ea2a8e8883ac0201c69f1872f1dd6292b1
Greg Schuman <gcschuman@hotmail.com>
2020-03-22 13:33:37 -0700

    Added most of the structure of the app-config, controllers, db, models, node modules, assets, server, views, and package and package lock json.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (name) VALUES ('Donut Burger');
+INSERT INTO burgers (name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (name) VALUES ('Chicken Burger');
+INSERT INTO burgers (name) VALUES ('Plain Hamburger');
+INSERT INTO burgers (name) VALUES ('Double-Double');
+INSERT INTO burgers (name) VALUES ('Quarter Pound Burger');
+INSERT INTO burgers (name) VALUES ('Fish Sandwich');
