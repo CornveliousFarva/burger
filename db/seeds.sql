e0cc3ee4ca303f1b7d87954d81bbde67de196e9a
Greg Schuman <gcschuman@hotmail.com>
2020-06-20 10:42:14 -0700

    Updated schema and seeds in the db folder.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,3 @@
-INSERT INTO burgers (name) VALUES ('Donut Burger');
-INSERT INTO burgers (name) VALUES ('Double Bacon Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Veggie Burger');
-INSERT INTO burgers (name) VALUES ('Chicken Burger');
-INSERT INTO burgers (name) VALUES ('Plain Hamburger');
-INSERT INTO burgers (name) VALUES ('Double-Double');
-INSERT INTO burgers (name) VALUES ('Quarter Pound Burger');
-INSERT INTO burgers (name) VALUES ('Fish Sandwich');
-INSERT INTO burgers (name) VALUES ('New Bacon-Ings');
-INSERT INTO burgers (name) VALUES ('Rest In Peas Burger');
-INSERT INTO burgers (name) VALUES ('Salvador Cauliflower Burger');
-INSERT INTO burgers (name) VALUES ('Poutine on the Ritz Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
